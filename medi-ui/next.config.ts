cf22e86cd7bf30bd896301d02715b0086a0b6b8b
Marcello DeLuca <delucam14@gmail.com>
2025-08-30 23:20:59 -0400

    update config and package

--- medi-ui/next.config.ts
+++ medi-ui/next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
